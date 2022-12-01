fc5e9ef58b60b044554edccec85aba1019280a17
Anant Jain <ritujainanant2810@gmail.com>
2022-12-01 16:13:21 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+module.exports = {
+  trailingSlash: true,
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' }
+    };
+  },
+  images: {
+    unoptimized: true
+  }
+};
 
-module.exports = nextConfig
