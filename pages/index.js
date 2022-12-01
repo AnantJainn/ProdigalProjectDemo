006c9952ffeccbb52aa051cbb8fb468361cb0bfb
Anant Jain <ritujainanant2810@gmail.com>
2022-12-01 16:35:17 +0530

    name change

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Algosage
+          Algosage Doc
         </h1>
 
         <p className={styles.description}>
